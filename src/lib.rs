0493e1d5f1431e0c2a29758aab6203f55ba2d3fc
sankitshane <sankit.acharya@gmail.com>
2024-06-17 14:19:39 +0530

    feat: Made json reader generic and read readings JSON

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use serde::de::DeserializeOwned;
 use std::fs::File;
 use std::io::BufReader;
 use serde_json::Result as SResult;
@@ -12,6 +13,13 @@ pub struct Config {
     pub energy_rate: f32
 }
 
+#[derive(Deserialize, Debug)]
+pub struct Readings {
+    pub dinesh_reading: f32,
+    pub sachin_reading: f32,
+    pub sankit_reading: f32
+}
+
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() != 2 {
@@ -21,26 +29,42 @@ impl Config {
         let file_path = args[1].clone();
         let output = match read_config_from_file(file_path) {
             Ok(config) => {
-                my_function(&config);
+                print_config(&config);
                 Ok(config)
             }
             Err(e) => Err("Error reading json file"),
         };
+
+        let current_path = "reading.json".to_string();
+        let current = match read_config_from_file(current_path) {
+            Ok(config) => {
+                print_reading(&config);
+                Ok(config)
+            }
+            Err(e) => Err("Error reading json file"),
+        };
+
         output
     }
 }
 
-fn read_config_from_file(file_path: String) -> SResult<Config> {
+fn read_config_from_file<T: DeserializeOwned>(file_path: String) -> SResult<T> {
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
-    let config = serde_json::from_reader(reader).unwrap();
-    Ok(config)
+    let content = serde_json::from_reader(reader).unwrap();
+    Ok(content)
 }
 
-fn my_function(config: &Config) {
+fn print_config(config: &Config) {
     println!("fixed_charge: {}", config.fixed_charge);
     println!("fppca_charge: {}", config.fppca_charge);
     println!("tax: {}", config.tax);
     println!("energy_unit: {}", config.energy_unit);
     println!("energy_rate: {}", config.energy_rate);
+}
+
+fn print_reading(config: &Readings) {
+    println!("dinesh_reading: {}", config.dinesh_reading);
+    println!("sachin_reading: {}", config.sachin_reading);
+    println!("sankit_reading: {}", config.sankit_reading);
 }
